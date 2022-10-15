496f213de4d23e1130515e0839fe116931ab50db
Zaytsevie <115174587+Zaytsevie@users.noreply.github.com>
2022-10-15 17:05:29 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const carName = "Opel Astra H"
+console.log("Марка автомобиля", carName)
+let maxCarSpeed = "160 km/h"
+console.log("Скорость машины", maxCarSpeed)
+maxCarSpeed = "150 km/h"
+console.log("У машины изменилась скорость", maxCarSpeed)
+maxCarSpeed = "140 km/h"
+console.log("У машины изменилась скорость", maxCarSpeed)
+maxCarSpeed = "130 km/h"
+console.log("У машины изменилась скорость", maxCarSpeed)
+
+let carOwner = "Igor"
+console.log("Владелелц машины", carOwner)
+carOwner = "Andrey"
+console.log("У машины изменился владелец", carOwner)
+carOwner = "kiril"
+console.log("У машины изменился владелец", carOwner)
+carOwner = "Alex"
+console.log("У машины изменился владелец", carOwner)
